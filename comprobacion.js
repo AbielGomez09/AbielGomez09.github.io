aaf0f5a318557030f0b14cca8cb64f0723008359
AbielGomez09 <156268416+AbielGomez09@users.noreply.github.com>
2024-01-25 20:22:25 -0600

    Add files via upload

---
+++ comprobacion.js
@@ -0,0 +1,32 @@
+function validacionExpre() {
+    var emailInput = document.getElementById('email');
+    var password = document.getElementById('password');
+    var emailvalidacion = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+
+    if (!emailvalidacion.test(emailInput.value)) {
+      window.alert('Ingresa un correo electrónico válido.');    
+      console.log('Ingresa un correo electrónico válido.');
+      
+      
+      return false;
+
+      
+
+
+    } 
+    
+    
+    if (password.value.length < 8) {
+        window.alert('La contraseña no cumple con el minimo carcter requerido');
+    
+        return false;
+      }
+
+
+    else {
+      window.alert('Sesion iniciada')
+      return true;
+    }  }
+
+
+  
